e6991a758b3864d319baefb57dbf031a92a71fd0
Brezerek <Brezerek@users.noreply.github.com>
2016-04-20 20:59:26 +0200

    Update baza_nazw.R

--- baza_nazw.R
+++ baza_nazw.R
@@ -1,12 +1,12 @@
 druzyny<-data.frame(nazwa=rep("a",30),poprzed.naz=rep(NA,30),rok.zmian=rep(NA,30),skrot=rep('a',30))
-druzyny$nazwa<-c("Atlanta Hawks","Boston Celtics","Brooklyn Nets","Charlotte Bobcats","Chicago Bulls",
+druzyny$nazwa<-c("Atlanta Hawks","Boston Celtics","Brooklyn Nets","Charlotte Hornets","Chicago Bulls",
                  "Cleveland Cavaleries","Dallas Mavericks","Denver Nuggets","Detroit Pistons",
                  "Golden State Warriors","Houston Rockets","Indiana Pacers","Los Angeles Clippers",
                  "Los Angeles Lakers","Memphis Grizzlies","Miami Heat","Milwaukee Bucks",
                  "Minnesota Timberwolves","New Orleans Pelicans",'New York Knicks',"Oklahoma City Thunder",
                  "Orlando Magic","Philadelphia 76ers","Phoenix Suns",'Portland Trail Blazers',
                  "Sacramento Kings","San Antonio Spurs","Toronto Raptors","Utah Jazz","Washington Wizards")
-druzyny$poprzed.naz<-c(NA,NA,"New Jersey Nets","Charlotte Hornets",NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,
+druzyny$poprzed.naz<-c(NA,NA,"New Jersey Nets","Charlotte Bobcats",NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,
                         "New Orleans Hornets",NA,"Seattle SuperSonics",NA,NA,NA,NA,NA,NA,NA,NA,NA)
 druzyny$rok.zmian<-c(NA,NA,2013,2015,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,2014,NA,2009,
                        NA,NA,NA,NA,NA,NA,NA,NA,NA)
