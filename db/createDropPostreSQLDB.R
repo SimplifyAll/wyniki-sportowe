3254d2a285e55118df16cec4653a5ff40990a136
Simplify.All <dstemach13@gmail.com>
2016-04-28 18:14:41 +0200

    nowa baza

--- db/createDropPostreSQLDB.R
+++ db/createDropPostreSQLDB.R
@@ -31,10 +31,11 @@ WITH (
 dbSendQuery(conn=con, statement = sql_command)
 
 sql_command <- "CREATE TABLE teams (
-  id            SERIAL,
-  current_name  varchar(8),
-  previous_name varchar(8),
+  current_name  varchar(30),
+  id            varchar(4) NOT NULL,
   change_year   varchar(4),
+  previous_name varchar(30),
+  previous_abr  varchar(8),
   CONSTRAINT teams_pk PRIMARY KEY (id)
 )
 WITH (
@@ -53,48 +54,79 @@ dbSendQuery(conn=con, statement = sql_command)
 
 sql_command <- "CREATE TABLE team_season_statistics (
   team_id                       varchar(4) NOT NULL,
-  season                        integer NOT NULL,
-  wins_num                      integer,
-  losses_num                    integer,
-  season_finish_position        integer,
-  srs                           real,
-  pace                          real,
-  offensive_rating              real,
-  defensive_rating              real,
-  pergame_field_goals           real,
-  pergame_field_goal_attempts   real,
-  pergame_3Ps                   real,
-  pergame_3P_attempts           real,
-  pergame_2Ps                   real,
-  pergame_2P_attempts           real,
-  pergame_free_throws           real,
-  pergame_free_throw_attempts   real,
-  pergame_offensive_rebounds    real,
-  pergame_defensive_rebounds    real,
-  pergame_assists               real,
-  pergame_steals                real,
-  pergame_blocks                real,
-  pergame_turnovers             real,
-  pergame_personal_fauls        real,
-  season_field_goals            integer,
-  season_field_goal_attempts    integer,
-  season_3Ps                    integer,
-  season_3P_attempts            integer,
-  season_2Ps                    integer,
-  season_2P_attempts            integer,
-  season_free_throws            integer,
-  season_free_throw_attempts    integer,
-  season_offensive_rebounds     integer,
-  season_defensive_rebounds     integer,
-  season_assists                integer,
-  season_steals                 integer,
-  season_blocks                 integer,
-  season_turnovers              integer,
-  season_personal_fauls         integer,
-  CONSTRAINT team_season_stats_pk PRIMARY KEY (team_id, season)
+game_num                      integer,
+min_play                      integer,
+field_goals                   integer,
+field_goals_a                 integer,
+field_goals_p                 integer,
+pergame_3Ps                   real,
+pergame_3P_attempts           real,
+pergame_3P_p                  real,
+pergame_2Ps                   real,
+pergame_2P_attempts           real,
+pergame_2P_p                  real,
+pergame_free_throws           real,
+pergame_free_throw_attempts   real,
+pergame_free_throw_p           real,
+pergame_offensive_rebounds    real,
+pergame_defensive_rebounds    real,
+total_defensive_rebounds      real,
+pergame_assists               real,
+pergame_steals                real,
+pergame_blocks                real,
+pergame_turnovers             real,
+pergame_personal_fauls        real,
+points_general                real,
+pergame_points                real,
+op_field_goals                   integer,
+op_field_goals_a                 integer,
+op_field_goals_p                 integer,
+op_pergame_3Ps                   real,
+op_pergame_3P_attempts           real,
+op_pergame_3P_p                  real,
+op_pergame_2Ps                   real,
+op_pergame_2P_attempts           real,
+op_pergame_2P_p                  real,
+op_pergame_free_throws           real,
+op_pergame_free_throw_attempts   real,
+op_pergame_free_throw_p           real,
+op_pergame_offensive_rebounds    real,
+op_pergame_defensive_rebounds    real,
+op_total_defensive_rebounds      real,
+op_ pergame_assists               real,
+op_ pergame_steals                real,
+op_pergame_blocks                real,
+op_pergame_turnovers             real,
+op_pergame_personal_fauls        real,
+op_ points_general                real,
+op_pergame_points                real,
+age                              real,
+pred_win                          real,
+pred_lose                         real,
+margin_v                          real,
+str_s                             real,
+simple_rate                       real,
+off_rate                          real,
+deff_rate                         real,
+pace_f                            real,
+free_throw_rate                   real,
+3p_rate                           real,
+true_shoot                        real,
+eff_goals                         real,
+turnover_p                        real,
+off_reb_p                         real,
+perfield_free_throw_p              real,
+opp_eff_goals_p                   real,
+opp_turnover_p                    real,
+def_reb_p                         real,
+opp_perfield_goal_attempt         real,
+arena                             varchar(50),
+attendance                        real,
+season                        varchar(8) NOT NULL,
+CONSTRAINT team_season_stats_pk PRIMARY KEY (team_id, season)
 )
 WITH (
-    OIDS=FALSE
+OIDS=FALSE
 );"
 dbSendQuery(conn=con, statement = sql_command)
 sql_command <- "ALTER TABLE team_season_statistics
@@ -105,29 +137,14 @@ ADD CONSTRAINT season_fkey FOREIGN KEY (season) REFERENCES seasons(yearStart);"
 dbSendQuery(conn=con, statement = sql_command)
 
 sql_command <- "CREATE TABLE games (
-  id                        serial,
+  id                        integer,
+  team_1_id               varchar(4) NOT NULL,
+  team_2_id               varchar(4) NOT NULL,
   game_date                 date,
-  is_team_1_home            boolean,
-  team_1_id               integer NOT NULL,
-  team_2_id               integer NOT NULL,
   team_1_score              integer,
   team_2_score              integer,
+  if_win                    character,
   season                    varchar(4) NOT NULL,
-  game_field_goals               integer,
-  game_filed_goal_attempts       integer,
-  game_2Ps                       integer,
-  game_2P_attempts               integer,
-  game_3Ps                       integer,
-  game_3P_attempts               integer,
-  game_free_throws               integer,
-  game_free_throw_attempts       integer,
-  game_offensive_rebounds        integer,
-  game_defensive_rebounds        integer,
-  game_assists                   integer,
-  game_steals                    integer,
-  game_blocks                    integer,
-  game_turnovers                 integer,
-  game_personal_fauls            integer,
   CONSTRAINT games_pk PRIMARY KEY (id)
 )
 WITH (
@@ -163,6 +180,17 @@ sql_command <- "ALTER TABLE bets
 ADD CONSTRAINT bet_firm_fkey FOREIGN KEY (bet_firm) REFERENCES betting_firms(name);"
 dbSendQuery(conn=con, statement = sql_command)
 
+
+# INSERT
+#dbSendPreparedQuery(con, "INSERT INTO teams() VALUES (?,?,?,?,?)", team.names) w przypadku 
+dbWriteTable(con, "teams", 
+             value = team.names, append = TRUE, row.names = FALSE)
+games.story <- data.frame(id=t(t(c(1:nrow(games.story)))),games.story[,-7])
+dbWriteTable(con, "seasons", value = data.frame(season=as.character(c(2011:2015))), append = TRUE, row.names = FALSE)
+dbWriteTable(con, "games", value = games.story[,-7], append = TRUE, row.names = FALSE)
+
+
+
 #DROP DB TABLES
 sql_command <- "DROP TABLE IF EXISTS bets;"
 dbSendQuery(conn=con, statement=sql_command)
@@ -178,3 +206,4 @@ sql_command <- "DROP TABLE IF EXISTS teams;"
 dbSendQuery(conn=con, statement=sql_command)
 
 dbDisconnect(con)
+
