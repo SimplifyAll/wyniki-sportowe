8c8d1769f3ad2d9e054d5c411611f86553f87b26
smudap <smudap@student.mini.pw.edu.pl>
2016-11-15 19:16:18 +0100

    package update

--- NBAResults/R/read_database.R
+++ NBAResults/R/read_database.R
@@ -20,9 +20,9 @@
 
 
 read_database <- function(connection){
-   assign("games_story", read_teams_names_table(connection), env = .GlobalEnv)
+   assign("teams_names", read_teams_names_table(connection), env = .GlobalEnv)
    assign("seasons", read_seasons_table(connection), env = .GlobalEnv)
-   assign("teams_names", read_games_story_table(connection), env = .GlobalEnv)
+   assign("games_story", read_games_story_table(connection), env = .GlobalEnv)
    assign("teams_seasons_statistics", read_teams_seasons_statistics_table(connection),
           env = .GlobalEnv)
    assign("bets_history", read_bets_history_table(connection), env = .GlobalEnv)
