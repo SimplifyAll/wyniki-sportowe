9fbe29c4c429103ce9153bc7305526803da74251
SimplifyAll <dstelmach13@gmail.com>
2017-01-07 16:11:52 +0100

    score_plot modified (subplots)

--- plots_D/score_plot.R
+++ plots_D/score_plot.R
@@ -5,42 +5,107 @@ library(stringi)
 library(plotly)
 
 year = 2014
+
+season_score <- teams_seasons_statistics %>%
+  full_join(teams_names, by = c("team_id" = "id")) %>%
+  filter(season == year) %>%
+  select(team_id, total_2ps, total_3ps, Possition, Conference) %>%
+  arrange(desc(Conference), Possition)
+
+teams_levels <- as.character(season_score$team_id)
+season_score$team_id <- factor(season_score$team_id, levels = teams_levels, ordered = TRUE)
+
+season_score_W <- season_score %>%
+  filter(Conference == 'W')
+
+season_score_E <- season_score %>%
+  filter(Conference == 'E')
+
+pW <- plot_ly(season_score_W, x = ~team_id, y = ~total_3ps, type = 'bar', name = '3 Points Shots Score, W') %>%
+  add_trace(y = ~total_2ps , name = '2 Points Shots Score, E') %>%
+  layout(barmode = 'stack')
+
+pE <- plot_ly(season_score_E, x = ~team_id, y = ~total_3ps, type = 'bar', name = '3 Points Shots Score, E') %>%
+  add_trace(y = ~total_2ps , name = '2 Points Shots Score, E') %>%
+  layout(barmode = 'stack')
+
+subplot(pW, pE) %>%
+  layout(title = paste0('3P and 2P in ',year,' season'),
+         xaxis = list(title = 'Teams'),
+         yaxis = list(title = 'Points'))
+
+#---------------------------
+#plotly war
+#---------------------------
+
+# p <- plot_ly(season_score, y = ~team_id, x = ~total_3ps, type = 'bar', name = '3 Points Shots Score', orientation = 'h',
+#              marker = list(color = 
+#                              c('#006C9A', '#006C9A', '#006C9A', '#006C9A', '#006C9A',
+#                                '#006C9A', '#006C9A', '#006C9A', '#006C9A', '#006C9A',
+#                                '#006C9A', '#006C9A', '#006C9A', '#006C9A', '#006C9A',
+#                                '#00BEBE', '#00BEBE', '#00BEBE', '#00BEBE', '#00BEBE',
+#                                '#00BEBE', '#00BEBE', '#00BEBE', '#00BEBE', '#00BEBE',
+#                                '#00BEBE', '#00BEBE', '#00BEBE', '#00BEBE', '#00BEBE'))) %>%
+#   add_trace(x = ~total_2ps , name = '2 Points Shots Score',
+#             marker = list(color = c(	'#00F3E4', '#00F3E4', '#00F3E4', '#00F3E4', '#00F3E4',
+#                                      '#00F3E4', '#00F3E4', '#00F3E4', '#00F3E4', '#00F3E4',
+#                                      '#00F3E4', '#00F3E4', '#00F3E4', '#00F3E4', '#00F3E4',
+#                                      '#9FF9C1', '#9FF9C1', '#9FF9C1', '#9FF9C1', '#9FF9C1',
+#                                      '#9FF9C1', '#9FF9C1', '#9FF9C1', '#9FF9C1', '#9FF9C1',
+#                                      '#9FF9C1', '#9FF9C1', '#9FF9C1', '#9FF9C1', '#9FF9C1'))) %>%
+#   layout(title = paste0('3P and 2P in ',year,' season'),
+#          xaxis = list(title = 'Teams'),
+#          yaxis = list(title = 'Points'),
+#          barmode = 'stack')
+# p
+
+
+
 #wykres 1
 #iloĹ›Ä‡ zdobytych punktĂłw za dwa i trzy w sezonie zasadniczym z podziaĹ‚em na konferencje
 #
 #przygotowanie tabeli
-  season_score <- teams_seasons_statistics %>%
-    full_join(teams_names, by = c("team_id" = "id")) %>%
-    filter(season == year) %>%
-    select(team_id, total_2ps, total_3ps, Possition, Conference) %>%
-    #mutate(total_2ps = total_2ps + total_3ps) %>%
-    arrange(desc(Conference), Possition) %>%
-    rename(`Total Score` = total_2ps, `3 Points Shots Score` = total_3ps, Team = team_id)
+  # season_score <- teams_seasons_statistics %>%
+  #   full_join(teams_names, by = c("team_id" = "id")) %>%
+  #   filter(season == year) %>%
+  #   select(team_id, total_2ps, total_3ps, Possition, Conference) %>%
+  #   #mutate(total_2ps = total_2ps + total_3ps) %>%
+  #   arrange(desc(Conference), Possition) %>%
+  #   rename(`Total Score` = total_2ps, `3 Points Shots Score` = total_3ps, Team = team_id)
+  # 
+  # teams_levels <- as.character(season_score$Team)
+  # 
+  # season_score<-melt(season_score,id.vars=c("Team", "Possition", "Conference")) %>%
+  #   arrange(desc(variable)) %>%
+  #   mutate(value = as.numeric(as.character(value))) %>%
+  #   rename(Points = value, `Points Type` = variable)
+  # type_levels <- c("3 Points Shots Score", "Total Score")
+  # 
+  # season_score$Team <- factor(season_score$Team, levels = teams_levels, ordered = TRUE)
+  # season_score$`Points Type` <- factor(season_score$`Points Type`, levels = type_levels, ordered = TRUE)
+  # 
+  # #tworzenie wykresĂłW
+  # plot <- ggplot(season_score, aes(x=Team, y=Points, fill=`Points Type`, alpha=Conference)) +
+  #   geom_bar(stat="identity", colour="black") + 
+  #   scale_alpha_manual(values=c(0.4, 1)) +
+  #   ggtitle(paste0('3P and 2P in ',year,' season')) +
+  #   xlab('Teams') + ylab('Points') +
+  #   #theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  #   coord_flip()
+  # #plot
+  # ggplotly(plot)
+  # 
   
-  teams_levels <- as.character(season_score$Team)
-  
-  season_score<-melt(season_score,id.vars=c("Team", "Possition", "Conference")) %>%
-    arrange(desc(variable)) %>%
-    mutate(value = as.numeric(as.character(value))) %>%
-    rename(Points = value, `Points Type` = variable)
-  type_levels <- c("3 Points Shots Score", "Total Score")
-  
-  season_score$Team <- factor(season_score$Team, levels = teams_levels, ordered = TRUE)
-  season_score$`Points Type` <- factor(season_score$`Points Type`, levels = type_levels, ordered = TRUE)
-  
-  #tworzenie wykresĂłW
-  plot <- ggplot(season_score, aes(x=Team, y=Points, fill=`Points Type`, alpha=Conference)) +
-    geom_bar(stat="identity", colour="black") + 
-    scale_alpha_manual(values=c(0.4, 1)) +
-    ggtitle(paste0('3P and 2P in ',year,' season')) +
-    xlab('Teams') + ylab('Points') +
-    #theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-    coord_flip()
-  #plot
-  ggplotly(plot)
   
   
-#---------------------------
-#plotly war
-#---------------------------
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
